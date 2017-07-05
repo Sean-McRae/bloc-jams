fca979e59f56702a84d4f564b8a4a045cb4297ba
L1GH7 <officialhocc@users.noreply.github.com>
2017-07-05 14:23:52 -0400

    created landing.js

---
+++ landing.js
@@ -0,0 +1,15 @@
+var animatePoints = function() {
+
+      var points = document.getElementsByClassName('point');
+
+      var revealPoint = function(point){
+        points[point].style.opacity = 1;
+        points[point].style.transform = "scaleX(1) translateY(0)";
+        points[point].style.msTransform = "scaleX(1) translateY(0)";
+        points[point].style.WebkitTransform = "scaleX(1) translateY(0)";
+      }
+
+      for(var i = 0; i < points.length;i++){
+        revealPoint(i);
+      }
+};

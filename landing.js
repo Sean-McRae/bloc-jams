9ba41bc833f9eb04586cf8851dcd5b8af3f253f3
Sean McRae <officialhocc@gmail.com>
2017-07-08 17:51:44 -0400

    added utilties.js

--- landing.js
+++ landing.js
@@ -15,10 +15,12 @@ window.onload = function(){
   if(window.innerHeight > 950){
     animatePoints(pointsArray);
   }
+var sellingPoints = document.getElementsByClassName('selling-points')[0];
+var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
 window.addEventListener("scroll", function(event){
-    if (pointsArray[0].getBoundingClientRect().top <= 500) {
-        animatePoints(pointsArray);
-    }
+  if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+  }
 });
 }

d5806bd2ec541c675f44d5518814ed0969eacb79
L1GH7 <officialhocc@users.noreply.github.com>
2017-07-17 15:32:18 -0400

    Updated

--- collection.js
+++ collection.js
@@ -1,25 +1,29 @@
-var buildColletionItemTemplate = function(){
-  var template =     '<div class="collection-album-container column fourth">'
-    + '  <img src="album/01.png"/>'
-    + '  <div class="collection-album-info caption">'
-    + '    <p>'
-    + '      <a class="album-name" href="album.html"> The Colors </a>'
-    + '      <br/>'
-    + '      <a href="album.html"> Pablo Picasso </a>'
-    + '      <br/>'
-    + '      X songs'
-    + '      <br/>'
-    + '    </p>'
-    + '  </div>'
+var buildCollectionItemTemplate = function buildCollectionItemTemplate() {
+    var template =
+      '<div class="collection-album-container column fourth">'
+    + '   <img src="/album/01.png" />'
+    + '   <div class="collection-album-info caption">'
+    + '       <p>'
+    + '           <a class="album-name" href="/album.html"> The Colors </a>'
+    + '           <br>'
+    + '           <a href="/album.html"> Pablo Picasso </a>'
+    + '           <br>'
+    + '           X songs'
+    + '           <br>'
+    + '       </p>'
+    + '   </div>'
     + '</div>'
     ;
+
     return $(template);
 };
 
-$(window).load(function(){
-  var $collectionContainer = $('album-covers');
+$(window).load(function () {
+  var $collectionContainer = $('.album-covers');
   $collectionContainer.empty();
-  var $newThumbnai = buildColletionItemTemplate();
-  $collectionContainer.append($newThumbnai);
+
+  for (var i = 0; i < 12; i++) {
+    var $newThumbnail = buildCollectionItemTemplate();
+    $collectionContainer.append($newThumbnail);
   }
 });
